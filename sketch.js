57d16e82555ed432ba5b68abe2d5e39c9f91f0a3
nicosiy <nicosiysoo@gmail.com>
2015-09-04 18:00:08 -0400

    changed circle to rectangle

--- sketch.js
+++ sketch.js
@@ -5,14 +5,14 @@ function setup() {
 
 function draw() {
 	// clear the background
-	background(0, 0, 0);
+	background(100, 100, 100);
 
 	// set a fill color
-	fill(255, 255, 255);
+	fill(100, 255, 255);
 
 	// set a stroke color
 	stroke(255, 0, 0);
 
 	// draw a circle
-	ellipse(320, 180, 100, 100);
+	rect(320, 180, 100, 100);
 }
